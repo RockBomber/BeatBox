d31d41ad0ee6385cfca328a09e0f96f8190aa5e7
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-03-24 21:37:32 +0300

    Added basic Activity and Fragment

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+}
