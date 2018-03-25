612fafe90f857444b1c0fe5cae0512b6e9fd7943
V.Kuznetsov <v.kuznetsov@voskhod.ru>
2018-03-25 12:49:52 +0300

    Release Pool Sound on destroy app

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -39,6 +39,12 @@ public class BeatBoxFragment extends Fragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
     private class SoundHolder extends RecyclerView.ViewHolder {
 
         private ListItemSoundBinding mBinding;
